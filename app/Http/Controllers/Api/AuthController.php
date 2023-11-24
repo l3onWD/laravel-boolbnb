5c8cf25d83cc36073c35d4a152485ebc594dd70a
l3on <l3onwebdev@gmail.com>
2023-11-24 19:04:18 +0100

    added login validation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,20 +5,42 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     //
     public function login(Request $request)
     {
+        // Get credentials
+        $credentials = $request->all();
 
-        $credencials = $request->all();
+        // Validate
+        $validator = Validator::make(
+            $credentials,
+            [
+                'email' => 'required|email',
+                'password' => 'required|min:5'
+            ],
+            [
+                'email.required' => 'La mail è obbligatoria',
+                'email.email' => 'La mail inserita non è valida',
+                'password.required' => 'La password è obbligatoria',
+            ]
+        );
 
-        if (Auth::attempt($credencials)) {
+        // Validation Fail
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Attempt Login
+        if (Auth::attempt($credentials)) {
             return response(Auth::user(), 200);
         }
 
-        return response('login failed', 401);
+        // Unauthorized
+        return response('login-failed', 401);
     }
 
 
