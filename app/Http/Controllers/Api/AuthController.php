d57b9e8b189cedcab0bcc8d894c3dccac92a0f97
l3on <l3onwebdev@gmail.com>
2023-11-25 12:23:41 +0100

    added register logic

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,13 +3,67 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+
+    // Register
+    public function register(Request $request)
+    {
+
+        // Validate
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'nullable|string|max:255',
+                'surname' => 'nullable|string|max:255',
+                'date_of_birth' => 'nullable|date',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed|min:5',
+
+            ],
+            [
+                'name.string' => 'Il nome non è valido',
+                'name.max' => 'Il nome deve essere lungo massimo :max caratteri',
+
+                'surname.string' => 'Il cognome non è valido',
+                'surname.max' => 'Il cognome deve essere lungo massimo :max caratteri',
+
+                'date_of_birth.date' => 'La data di nascita non è valida',
+
+                'email.required' => 'La mail è obbligatoria',
+                'email.email' => 'La mail inserita non è valida',
+                'email.unique' => "Esiste già una mail chiamata $request->email",
+
+                'password.required' => 'La password è obbligatoria',
+                'password.confirmed' => 'Le password non coincidono',
+            ]
+        );
+
+        // Validation Fail
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Create user and crypt password
+        $user = User::create([
+            'name' => $request->name,
+            'surname' => $request->surname,
+            'email' => $request->email,
+            'date_of_birth' => $request->date_of_birth,
+            'password' => bcrypt($request->password)
+        ]);
+
+        // Confirm creation
+        return response($user, 201);
+    }
+
+
+    // Login
     public function login(Request $request)
     {
         // Get credentials
@@ -44,6 +98,7 @@ class AuthController extends Controller
     }
 
 
+    // Logout
     public function logout()
     {
 
